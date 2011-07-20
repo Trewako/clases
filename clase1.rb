323d8ecc2d9b826f650c3905a11223058b01266d
Demian Schopf <demian.schopf@gmail.com>
2011-07-19 20:56:57 -0400

    agregando comentarios

--- clase1.rb
+++ clase1.rb
@@ -1,11 +1,14 @@
 s = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-
+# asignar a la variable p un array generado por split (que es un método para strings)
 p = s.split
- 
+#iteramos los valores de p e imprimimos un string con la palabra interpolada --signo gato+llaves = #{o}-- 
 p.each{|o | puts "encontro la palabra #{o}"}
 
 
 ## iteracion encontrar occtaetac
+
+
+
 p.each do |o|
    puts "encontro #{o}" if o == "occaecat"
 end
@@ -15,7 +18,8 @@ claves = ["consectetur", "elit", "incididunt", "labore"]
 palabras_encontradas = []
 p.each do |o|
    if claves.include?(o)
-     puts "encontro #{o}" 
+     puts "encontro #{o}"
+     #agregamos elementos encontrados al array palabras encontradas.  
      palabras_encontradas << o
    end
 end
@@ -25,9 +29,10 @@ palabras_encontradas
 p.each{|o| puts "encontro #{o}" if claves.include?(o) }
 
 
-## un hash a = {"ropero"=>{} }
-a = {}
-a["ropero"]
+## un hash a = {"ropero"=>{} }. Para construir un hash y arrays con categorías y subcategorías (diccionarios de datos)
+
+a = {} # inicializamos el hash usando llave, se pone el concepto entre comillas y se encierra en corchete)
+a["ropero"] 
 a["ropero"]  = 1
 a["ropero"]  
 a["ropero"].class
